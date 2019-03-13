8836df2d6c8d259664fa41795cf125a4e79f86b2
Nicklas Burwell <Drol_Tromedlov@yahoo.com>
2019-03-13 18:48:42 -0400

    Finshed Hangman project

--- hangman/game.py
+++ hangman/game.py
@@ -1,23 +1,55 @@
 from .exceptions import *
+import random
 
 # Complete with your own, just for fun :)
-LIST_OF_WORDS = []
+LIST_OF_WORDS = ['rmotr', 'Python', 'coding']
 
 
 def _get_random_word(list_of_words):
-    pass
+    if not list_of_words:
+        raise InvalidListOfWordsException
+    return random.choice(list_of_words)
 
 
 def _mask_word(word):
-    pass
+    if not word:
+        raise InvalidWordException
+    masked_word = ''
+    for char in word:
+        masked_word += "*"
+    return masked_word
 
 
 def _uncover_word(answer_word, masked_word, character):
-    pass
+    if not answer_word or len(answer_word) != len(masked_word):
+        raise InvalidWordException
+    if not character or len(character) > 1:
+        raise InvalidGuessedLetterException
+
+    guessed_word = ""
+    for index, letter in enumerate(answer_word):
+        if letter.lower() == character.lower():
+            guessed_word += letter.lower()
+        else:
+            guessed_word += masked_word[index]
+    return guessed_word
+
+
 
 
 def guess_letter(game, letter):
-    pass
+    if game['masked_word'] == game['answer_word'] or game['remaining_misses'] <= 0:
+        raise GameFinishedException
+
+    game['masked_word'] = _uncover_word(game['answer_word'], game["masked_word"], letter)
+    game['previous_guesses'].append(letter.lower())
+    if letter.lower() not in game['masked_word']:
+        game['remaining_misses'] -= 1
+    if game['remaining_misses'] <= 0:
+        raise GameLostException
+    if game['masked_word'] == game['answer_word']:
+        raise GameWonException
+
 
 
 def start_new_game(list_of_words=None, number_of_guesses=5):
